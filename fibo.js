78fe7b6b1684d722211ab80d154867786508184a
talassael <talassael@gmail.com>
2023-01-31 08:07:00 +0200

    created loop solution

--- fibo.js
+++ fibo.js
@@ -1,6 +1,6 @@
 function calculate_fibo() {
     var user_input = document.getElementById("userinput").value;
-    var result = fiborec(user_input);
+    var result = fiboloop(user_input);
     document.getElementById("fibo_result").innerHTML = result;
 }
 
@@ -10,4 +10,20 @@ function fiborec(aaa) {
     } else {
         return fiborec(aaa - 1) + fiborec(aaa - 2);
     }
+}
+
+function fiboloop(aaa) {
+    if (aaa == 1) {
+        return 0;
+    } else if (aaa == 2) {
+        return 1;
+    }
+    var a = 0;
+    var b = 1;
+    for (let i = 3; i <= aaa; i++) {
+        var c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
